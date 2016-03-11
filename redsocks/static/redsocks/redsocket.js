45876e5d93595c3da94d9d5c42b652bb4bf55a91
Michael Shepanski <mjs7231@gmail.com>
2016-03-11 00:23:02 -0500

    Calculate latency from heartbeats

--- redsocks/static/redsocks/redsocket.js
+++ redsocks/static/redsocks/redsocket.js
@@ -8,10 +8,13 @@ function Redsocket(options) {
   if (options.uri === undefined)
 		throw new Error('No Websocket URI in options');
 
-  var ws, deferred, timer;
-  var attempts = 1;
-  var heartbeat_interval = null;
-  var missed_heartbeats = 0;
+  var ws, deferred;               // websocket and deferred objects
+  var connect_attempts = 1;       // attempts to create socket connection
+  var connect_timer = null;       // connection timer
+  var heartbeat_sent = null;      // timestamp last heartbeat sent
+  var heartbeat_timer = null;     // heartbeat timer
+  var heartbeats_missed = 0;      // count of heartbeats missed
+  var latency = null;             // current latency in ms
   var opts = $.extend({heartbeat_msg:null}, options);
   connect(opts.uri);
   
@@ -23,7 +26,7 @@ function Redsocket(options) {
 			ws.onmessage = on_message;
 			ws.onerror = on_error;
 			ws.onclose = on_close;
-			timer = null;
+			connect_timer = null;
 		} catch (err) {
 			deferred.reject(new Error(err));
 		}
@@ -31,24 +34,26 @@ function Redsocket(options) {
   
   function send_heartbeat() {
 		try {
-			missed_heartbeats += 1;
-			if (missed_heartbeats > 3)
+			heartbeats_missed += 1;
+			if (heartbeats_missed > 3)
 				throw new Error('Too many missed heartbeats.');
+      heartbeat_sent = Date.now();
 			ws.send(opts.heartbeat_msg);
 		} catch(err) {
-			clearInterval(heartbeat_interval);
-			heartbeat_interval = null;
+			clearInterval(heartbeat_timer);
+			heartbeat_timer = null;
 			console.warn('Closing connection: '+ err.message);
 			ws.close();
 		}
 	}
   
   function on_open() {
-		attempts = 1;
+		connect_attempts = 1;
 		deferred.resolve();
-		if (opts.heartbeat_msg && heartbeat_interval === null) {
-			missed_heartbeats = 0;
-			heartbeat_interval = setInterval(send_heartbeat, 5000);
+		if (opts.heartbeat_msg && heartbeat_timer === null) {
+			heartbeats_missed = 0;
+			heartbeat_timer = setInterval(send_heartbeat, 5000);
+      setTimeout(send_heartbeat, 100);
 		}
 		if ($.type(opts.connected) === 'function') {
 			opts.connected();
@@ -57,10 +62,10 @@ function Redsocket(options) {
 
 	function on_close(evt) {
 		console.warn('Connection closed.');
-		if (!timer) {
-			var interval = generate_inteval(attempts);
-			timer = setTimeout(function() {
-				attempts += 1;
+		if (!connect_timer) {
+			var interval = generate_interval(connect_attempts);
+			connect_timer = setTimeout(function() {
+				connect_attempts += 1;
 				connect(ws.url);
 			}, interval);
 		}
@@ -73,16 +78,18 @@ function Redsocket(options) {
 
 	function on_message(evt) {
 		if (opts.heartbeat_msg && evt.data === opts.heartbeat_msg) {
-			missed_heartbeats = 0;
+			latency = Date.now() - heartbeat_sent;
+      heartbeats_missed = 0;
+      return opts.receive_heartbeat(latency);
 		} else if ($.type(opts.receive_message) === 'function') {
 			return opts.receive_message(evt.data);
 		}
 	}
   
-  function generate_inteval(k) {
-		var maxInterval = (Math.pow(2, k) - 1) * 1000;
-    maxInterval = Math.min(maxInterval, 30*1000);
-		return Math.random() * maxInterval;
+  function generate_interval(k) {
+		var maxint = (Math.pow(2, k) - 1) * 1000;
+    maxint = Math.min(maxint, 30*1000);
+		return Math.random() * maxint;
 	}
   
   this.send_message = function(message) {
